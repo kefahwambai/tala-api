dccb354001397de30bb49f04bfa55b415183ba5e
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-10-02 10:02:03 +0300

    updated localhost origin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   config.require_master_key = true
   config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' =>  "https://talaapp.vercel.app/",
+    'Access-Control-Allow-Origin' =>  "http://localhost:3000",
     'Access-Control-Allow-Methods' => 'POST, PUT, DELETE, GET, OPTIONS',
     'Access-Control-Allow-Headers' => 'Origin, Content-Type, Accept, Authorization, Token',
     'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(","),
