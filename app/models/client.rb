9a9948ceafbfe08457361c17268a75e6ce12104a
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-10-02 15:33:15 +0300

    removed avatar validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,10 +5,10 @@ class Client < ApplicationRecord
   has_many_attached :images
 
   validates :name, presence: true, uniqueness: true
-  validates :avatar, attached: true, dimension: { width: { min: 800, max: 2400 } },
-    content_type: [:png, :jpg, :jpeg], size: { less_than: 100.kilobytes , message: 'is not given between size' }
+  # validates :avatar, attached: true, dimension: { width: { min: 800, max: 2400 } },
+  #   content_type: [:png, :jpg, :jpeg], size: { less_than: 100.kilobytes , message: 'is not given between size' }
 
-  validates :images, attached: true, limit: { min: 1, max: 3 }, content_type: [:png, :jpg, :jpeg, :mp3]
+  # validates :images, attached: true, limit: { min: 1, max: 3 }, content_type: [:png, :jpg, :jpeg, :mp3]
 
 
   def thumbnail
