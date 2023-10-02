adf648ef420295ca6755fb773c7e0086ca70bf43
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-10-02 11:48:05 +0300

    rectified error image_url

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,13 +4,13 @@ class Client < ApplicationRecord
     has_many :loans
     has_one_attached :image
 
-      ddef image_url
+    def image_url
       if image.attached?
         Rails.application.routes.url_helpers.url_for(image.variant(resize: '100x100'), host: 'https://topacash.onrender.com')
       else
         nil
       end
-    end  
-      
+    end   
+         
     
 end
