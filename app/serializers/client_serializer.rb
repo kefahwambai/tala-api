06df4dc0447af10d38acfc4c6b07a3a4a0226617
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-10-02 14:46:12 +0300

    avatar serializer

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -3,4 +3,10 @@ class ClientSerializer < ActiveModel::Serializer
   attributes :id, :name, :avatar_url
   has_many :loans
   
+  def avatar_url
+    if object.avatar.attached?
+      Rails.application.routes.url_helpers.rails_blob_url(object.avatar, only_path: true)
+    else
+      nil
+    end
 end
