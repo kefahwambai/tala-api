c6f26512eb01904ac2e18bd35a770441227a2b63
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-10-02 11:44:07 +0300

    image_url correction

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,23 +4,13 @@ class Client < ApplicationRecord
     has_many :loans
     has_one_attached :image
 
-    def image_url
+      ddef image_url
       if image.attached?
-        Rails.application.routes.url_helpers.url_for(image, host: 'https://topacash.onrender.com')
+        Rails.application.routes.url_helpers.url_for(image.variant(resize: '100x100'), host: 'https://topacash.onrender.com')
       else
         nil
       end
-    end
-    
-    
-
-    def thumbnail
-        if image.attached?
-          image.variant(resize: '100x100').processed
-        else
-          nil
-        end
-      end
+    end  
       
     
 end
