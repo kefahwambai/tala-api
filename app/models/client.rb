ae8c20f50c8644e40750953a411972dd5769118f
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-10-02 12:01:20 +0300

    image erros

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,22 @@
 class Client < ApplicationRecord
-    validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 
-    has_many :loans
-    has_one_attached :avatar   
-    
+  has_many :loans
+  has_one_attached :avatar
+
+  def image_url
+    if avatar.attached?
+      Rails.application.routes.url_helpers.url_for(avatar.variant(resize: '100x100', host: 'https://topacash.onrender.com'))
+    else
+      nil
+    end
+  end
+
+  def thumbnail
+    if avatar.attached?
+      avatar.variant(resize: '100x100').processed
+    else
+      nil
+    end
+  end
 end
